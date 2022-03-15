e7c20e8ef89e6da855957745fb9a360d35c1eedc
Noah V <noah@noahvandenberg.com>
2022-03-15 15:35:44 -0700

    rspec user spec

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,21 @@
 class User < ActiveRecord::Base
 
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: true }
+  validates :password, presence: true, length: { minimum: 8 }
+  validates :password_confirmation, presence: true
+
   has_secure_password
+
+  def self.authenticate_with_credentials(email, password)
+
+    user = User.find_by_email(email.strip.downcase)
+    if (user && user.authenticate(password))
+      return user
+    else 
+      return false
+    end
+
+  end
 
 end
